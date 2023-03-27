78e22194cf111be5d39e58d4fded76493fda02d3
Hieu Tran <hieutran@MacBook-Pro-466.local>
2023-03-26 23:01:55 -0400

    Connect to database & Create User table
    Signed-off-by: Hieu Tran <hieutran@MacBook-Pro-466.local>

--- BobaSpot/BobaSpot/BobaSpot/settings.py
+++ BobaSpot/BobaSpot/BobaSpot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'BobaSpot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST_IP'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
+AUTH_USER_MODEL = 'backend.CustomUser'
 
 
 # Password validation
