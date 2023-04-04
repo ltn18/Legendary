227a9bc83dad7d9f7ac0face16b556e8327eef24
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-03 23:53:31 -0400

    add validation, change font size and button color
    Signed-off-by: Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>

--- BobaSpot/BobaSpot/frontend/src/components/Auth/Auth.js
+++ BobaSpot/BobaSpot/frontend/src/components/Auth/Auth.js
@@ -5,6 +5,7 @@ import "./Auth.css";
 
 
 const { Title } = Typography;
+
 const initialFormState = {
   firstname: '',
   lastname: '',
@@ -13,13 +14,59 @@ const initialFormState = {
 }
 
 function Auth() { 
+  const minLevel = 0.1;
+  const errorMessage = 'Password is too weak';
+  const [level,setLevel]=useState(0)
   const [isLogin, setIsLogin] = useState(true);
   const [form, setForm] = useState(initialFormState);
+  const [passwordStrength, setPasswordStrength] = useState(null);
+
+  const checkPasswordStrength = (rule, value, callback) => {
+    if (value && value.length >= 8) {
+      let strength = 0;
+      if (value.match(/[a-z]/)) {
+        strength += 1;
+      }
+      if (value.match(/[A-Z]/)) {
+        strength += 1;
+      }
+      if (value.match(/[0-9]/)) {
+        strength += 1;
+      }
+      if (value.match(/[^a-zA-Z0-9]/)) {
+        strength += 1;
+      }
+
+      if (strength === 0) {
+        callback('Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character!');
+      } else if (strength === 1) {
+        setPasswordStrength('Weak');
+        callback();
+      } else if (strength === 2) {
+        setPasswordStrength('Medium');
+        callback();
+      } else if (strength === 3) {
+        setPasswordStrength('Strong');
+        callback();
+      } else {
+        setPasswordStrength('Very Strong');
+        callback();
+      }
+    } else {
+      callback('Password must be at least 8 characters long!');
+    }
+  };
 
   const onFinish = (values) => {
     console.log('Received values of form:', values);
-    // need to mock api
-
+    /**
+     * MANUAL VALIDATOR
+     * Signup -> check fields (length, strength)
+     * - if username < 6 char 
+     * - if password < 10 char
+     * submit the data to backend
+     * backend will send back token if the user is valid
+     */
   };
 
   const toggleAuth = () => { 
@@ -28,23 +75,29 @@ function Auth() {
   return (
     <>
       <Row className='Full-page'>
+        {/* left column */}
         <Col className='Column' span={12} style={{}}>
           <img style={{ width: '100%', height: '100%' }} src={"https://cdn.dribbble.com/userupload/3841872/file/original-0a6f56e82ee816c6b6ab202747a58307.png?compress=1&resize=1024x768"} />
         </Col>
+        {/* right column */}
         <Col className='Column' span={12}>
           <Form
             name="login-signup"
             className="login-form"
             onFinish={onFinish}
           >
-            <Title className="logo-text">
+            <Title level={1} className="logo-text">
               Boba<br />Spot
             </Title>
+            {/* firstname and lastname */}
             {!isLogin &&
               <>
+              {/* firstname */}
                 <Form.Item
                   name="firstname"
                   className='form-field'
+                  validateStatus={String.validateStatus}
+                  help={String.errorMsg}
                   rules={[
                     {
                       required: true,
@@ -56,10 +109,14 @@ function Auth() {
                     placeholder="First name"
                     onChange={(e) => setForm({ ...form, firstname: e.target.value })}
                   />
+                  
                 </Form.Item>
+                {/* lastname */}
                 <Form.Item
                   name="lastname"
                   className='form-field'
+                  validateStatus={String.validateStatus}
+                  help={String.errorMsg}
                   rules={[
                     {
                       required: true,
@@ -75,40 +132,58 @@ function Auth() {
                 </Form.Item>
               </>
             }
+            {/* username */}
             <Form.Item
               name="username"
               className='form-field'
-              rules={[
+              validateStatus={String.validateStatus}
+              help={String.errorMsg}
+              rules={isLogin ? null : [
                 {
                   required: true,
                   message: 'Please input your Username!',
                 },
+                {
+                  min: 6, 
+                  message: 'Username must be minimum 6 characters',
+                },
+                {
+                  max:20, 
+                  message:'Username name must be less than 20 characters'
+                }
               ]}
             >
               <Input
                 placeholder="Username"
-                onChange={(e) => setForm({ ...form, username: e.target.value })}
+                onChange={(e) => setForm({ ...form, username: e.target.value})}
                 style={{borderRadius: '0px'}}
               />
             </Form.Item>
+            {/* password */}
             <Form.Item
               name="password"
-              rules={[
+              rules={isLogin ? null : [
                 {
                   required: true,
                   message: 'Please input your Password!',
                 },
-              ]}
+                {
+                  validator: checkPasswordStrength,
+                }
+              ]
+              }
+              extra={passwordStrength && `Password Strength: ${passwordStrength}`}
             >
               <Input
                 type="password"
                 placeholder="Password"
                 onChange={(e) => setForm({ ...form, password: e.target.value })}
+                onLevelChange={ setLevel}
                 style={{borderRadius: '0px'}}
               />
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-signup-button">{isLogin ? 'Log-in' : 'Sign Up'}</Button>
+              <Button htmlType="submit" className="login-signup-button">{isLogin ? 'Log-in' : 'Sign Up'}</Button>
               <Typography className='auth-toggle' onClick={toggleAuth}><u>{isLogin ? "Don't have an account? Signup!" : 'Already had an account? Log in'}</u></Typography>
             </Form.Item>
 
