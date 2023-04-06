aba561a2da180c6bc64e4ac20bf39cdd6550fdc3
Escanord <anhduy6102@gmail.com>
2023-04-06 18:15:12 -0400

    update testing with obtained token
    Signed-off-by: Escanord <anhduy6102@gmail.com>

--- BobaSpot/BobaSpot/backend/test/test_views.py
+++ BobaSpot/BobaSpot/backend/test/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from django.urls import reverse
 from backend import views
 from backend.models import CustomUser
@@ -10,7 +11,7 @@ def api_client():
 
 @pytest.fixture
 def test_user(db):
-    return CustomUser.objects.create(username='someone', hashpass="b'Yg=='", first_name='N', last_name='M', image_url='I')
+    return CustomUser.objects.create(username='someone', hashpass="b'YQ=='", first_name='N', last_name='M', image_url='I')
 
 @pytest.mark.django_db
 def test_unauthorized_request(api_client):
@@ -24,3 +25,14 @@ def test_log_in(api_client, test_user):
     response = api_client.post(url, {'username':'someone', 'password':'a'}, format='json')
     print(CustomUser.objects.get(username='someone').hashpass)
     assert response.status_code == 200
+    
+@pytest.mark.django_db
+def test_authorized_request(api_client, test_user):
+    url = reverse('login-view')
+    response = api_client.post(url, {'username':'someone', 'password':'a'}, format='json')
+    token = json.loads(response.data)['token']
+    api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+    url = reverse('test-view')
+    response = api_client.get(url)
+    assert response.status_code == 200
+    assert json.loads(response.data)['message'] == "hello Aiden!"
