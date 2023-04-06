20143a47409119698df3641555ef4af0e758e947
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-06 19:24:42 -0400

    sketch for bobaplace site
    Signed-off-by: Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>

--- BobaSpot/BobaSpot/frontend/src/App.js
+++ BobaSpot/BobaSpot/frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+// import NavBar from './components/layout/NavBar';
+import NavBar from './components/NavBar/NavBar';
+import ShopProfile from './components/ShopProfile/ShopProfile';
+// import UserProfile from './contents/profile/user/customer/UserProfile';
+// import ShopOwnerProfile from './contents/profile/user/shopowner/ShopOwnerProfile';
 
-function App() {
+// verify yourself, copilot
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <ShopProfile/>
+      {/* <Routes>
+        <Route exact path='/user' element={<UserProfile/>}/>
+        <Route exact path='/shopowner' element={<ShopOwnerProfile />}/>
+      </Routes> */}
+    </Router>
   );
 }
 
