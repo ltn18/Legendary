ec4ac15457e8fa5b2299b1a2169266238ec43aaa
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-23 13:47:05 -0400

    change shopprofile for admin, dropdown, and validation. add test

--- BobaSpot/BobaSpot/frontend/src/components/ShopProfile/ShopProfile.js
+++ BobaSpot/BobaSpot/frontend/src/components/ShopProfile/ShopProfile.js
@@ -1,9 +1,10 @@
-import { Col, Row, Image, Typography, Card, Form, Button, Rate, Input } from "antd";
+import { Col, Row, Image, Typography, Card, Form, Button, Rate, Select, Input } from "antd";
 import { StarOutlined, SendOutlined } from "@ant-design/icons";
 import "./ShopProfile.css";
 import { useEffect, useState } from "react";
 import TextArea from "antd/es/input/TextArea";
 import axios from "axios";
+const { Option } = Select;
 
 const initialFormState = {
     // profile_pic: "https://media.licdn.com/dms/image/D5603AQH503kJ6XqNxQ/profile-displayphoto-shrink_800_800/0/1666810388572?e=1686787200&v=beta&t=RZamxmNrhbyR1eWTHtlvUvaNTlyk9_aNhOXHYW9UL_U",
@@ -15,6 +16,12 @@ const initialFormState = {
 
 
 function ShopProfile() {
+    const [reviewForm, setReviewForm] = useState(initialFormState);
+    const [data, setData] = useState({});
+
+    const avatar = "https://media.licdn.com/dms/image/D5603AQH503kJ6XqNxQ/profile-displayphoto-shrink_800_800/0/1666810388572?e=1686787200&v=beta&t=RZamxmNrhbyR1eWTHtlvUvaNTlyk9_aNhOXHYW9UL_U";
+    const placeholder = "https://scontent.xx.fbcdn.net/v/t1.15752-9/329766254_5741560349290924_4541925126048218166_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=fs_zPy5csoQAX8k9vVU&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdStPVW_X4iWO-IslSvJKRRmdR_bIqbnOO-wu-GVV6zVYw&oe=646C02C8";
+
     const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6Ijc2MTIxNzA1LWU3ZjQtNDI4ZC1iOTk1LTUzYzNmY2QwMjVhMyIsInVzZXJuYW1lIjoiamFuaWNlIiwiaGFzaHBhc3MiOiJiJ2FtbDZhR1U9JyJ9.Aok-VWRAkraeWvzTiZRa0s69sM8cP_MFZWgCGM9BtaA\"}";
     const jwt2 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjZhMmRlMTU2LWQ2NzAtNDNlMC05NDM5LTVlNTVmYThlNzhmZSIsInVzZXJuYW1lIjoiRE0iLCJoYXNocGFzcyI6ImInWVE9PScifQ.OusPbgVk04hRQoMsvjkK7uKS0K2948jXQQIk0b-3BBs";
     const url1 = "http://127.0.0.1:8000/api/bobashop/fdc4875e-c552-486e-b6c3-a4e0d715eaed/";
@@ -30,14 +37,30 @@ function ShopProfile() {
         }
     };
 
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.request(getOptions)
+                .then(res => res.data)
+                .catch(error => console.log(error.response));
 
-    const [reviewForm, setReviewForm] = useState(initialFormState);
-    const [data, setData] = useState({});
+            result.opening_hour = result?.opening_hour?.slice(0, 5);
+            result.closing_hour = result?.closing_hour?.slice(0, 5);
 
-    const avatar = "https://media.licdn.com/dms/image/D5603AQH503kJ6XqNxQ/profile-displayphoto-shrink_800_800/0/1666810388572?e=1686787200&v=beta&t=RZamxmNrhbyR1eWTHtlvUvaNTlyk9_aNhOXHYW9UL_U";
+            const jsonResult = JSON.parse(JSON.stringify(result));
+            console.log(jsonResult);
+            setData(jsonResult);
+            console.log(data);
+        }
+        fetchData();
+    }, []);
 
-    const onSubmit = () => {
+
+
+
+    const onSubmitReview = () => {
+        if (!isValidForm()) {
+            return;
+        }
         const newData = JSON.parse(JSON.stringify(data));
         newData.reviews.unshift(reviewForm);
         setData(newData);
@@ -58,29 +81,16 @@ function ShopProfile() {
 
     const putReview = async () => {
         const result = await axios.request(putOptions)
-                        .then(res => res.data)
-                        .catch(error => console.log(error.response));
-        
+            .then(res => res.data)
+            .catch(error => console.log(error.response));
+
 
         console.log(result);
     }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios.request(getOptions)
-                .then(res => res.data)
-                .catch(error => console.log(error.response));
-    
-            result.opening_hour = result?.opening_hour?.slice(0, 5);
-            result.closing_hour = result?.closing_hour?.slice(0, 5);
-    
-            const jsonResult = JSON.parse(JSON.stringify(result));
-            console.log(jsonResult);
-            setData(jsonResult);
-            console.log(data);
-        }
-        fetchData();
-    }, []);
+    const isValidForm = () => {
+        return reviewForm.text !== "" && reviewForm.rating !== 0 && reviewForm.drink_name !== "";
+    }
 
     return (
         <>
@@ -155,8 +165,8 @@ function ShopProfile() {
                             ))
                         }
                     </Row>
-                    {/* input user's text */}
-                    <Row gutter={[10, 0]} className='grey-bg'>
+                    {/* REVIEW FORM */}
+                    {false || <Row gutter={[10, 0]} className='grey-bg'>
                         {/* avatar */}
                         <Col span={2}>
                             <Image src={avatar} />
@@ -165,21 +175,43 @@ function ShopProfile() {
                         <Col span={22}>
                             <Form style={{ backgroundColor: 'white', padding: '1em' }}>
                                 <Form.Item>
-                                    <TextArea style={{fontSize: '1.5em'}} placeholder="Write a text here" rows={4} value={reviewForm.text} onChange={(e) => setReviewForm({ ...reviewForm, text: e.target.value })} />
+                                    <TextArea
+                                        style={{ fontSize: '1.5em' }}
+                                        maxLength={500}
+                                        status={reviewForm.text === "" ? "error" : "success"}
+                                        placeholder="Write a text here"
+                                        rows={4} value={reviewForm.text}
+                                        onChange={(e) => setReviewForm({ ...reviewForm, text: e.target.value })} />
                                 </Form.Item>
                                 <Form.Item>
-                                    <Row style={{display: 'flex', justifyContent: 'space-between'}}>
+                                    <Row style={{ display: 'flex', justifyContent: 'space-between' }}>
                                         <Col style={{ fontSize: '1.5em' }}>
-                                            Drink Ordered <br />
-                                            <Input onChange={(e) => setReviewForm({ ...reviewForm, drink_name: e.target.value})} />
+                                            Drink ordered:<br />
+                                            <Select
+                                                style={{ width: '20em' }}
+                                                status={reviewForm.drink_name === "" ? "error" : "success"}
+                                                value={reviewForm.drink_name}
+                                                onChange={value => setReviewForm({ ...reviewForm, drink_name: value })}>
+                                                <Option value="Caffè macchiato">Caffè macchiato</Option>
+                                                <Option value="Pink drink">Pink drink</Option>
+                                            </Select>
                                         </Col>
                                         <Col style={{ fontSize: '1.5em' }}>
                                             Rating:<br />
-                                            <Rate style={{fontSize: '1.5em'}} onChange={(e) => setReviewForm({ ...reviewForm, rating: e})} />
+                                            <Rate
+                                                data-testid="rate-stars"
+                                                value={reviewForm.rating}
+                                                style={{ fontSize: '1.5em' }}
+                                                onChange={(e) => setReviewForm({ ...reviewForm, rating: e })} />
                                         </Col>
-                                        <Col style={{ display: 'flex', justifyContent: 'end'}}>
-                                            <Button type="primary" onClick={onSubmit} style={{ display:'flex', justifyContent:'center', alignItems:'center', height:'4em', width:'5em', backgroundColor: '#ffeeef', color: 'black' }}>
-                                                <SendOutlined style={{fontSize: '2.5em'}} />
+                                        <Col style={{ display: 'flex', justifyContent: 'end' }}>
+                                            <Button
+                                                aria-label="send"
+                                                type="primary"
+                                                id='submit-button'
+                                                onClick={onSubmitReview}
+                                                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '4em', width: '5em', backgroundColor: '#ffeeef', color: 'black' }}>
+                                                <SendOutlined style={{ fontSize: '2.5em' }} />
                                             </Button>
                                         </Col>
                                     </Row>
@@ -189,16 +221,17 @@ function ShopProfile() {
                             </Form>
                         </Col>
                     </Row>
-                    {/* show all reviews */}
+                    }
+                    {/* ALL REVIEWS */}
 
                     {
                         data?.reviews?.map((review, i) => (
                             <Row key={i} gutter={[10, 0]} className='grey-bg'>
                                 {/* avatar */}
                                 <Col span={2}>
-                                    <Image src={review.profile_pic} />
+                                    <Image src={review.profile_pic || placeholder} />
                                 </Col>
-                                {/* text text */}
+                                {/* review */}
                                 <Col span={22}>
                                     <Row>
                                         <Col span={24} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
