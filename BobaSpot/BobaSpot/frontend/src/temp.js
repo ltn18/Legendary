9699a669c73e0329a76b5e491484b8c8f5f7c10f
janicejzyh <93796231+janicejzyh@users.noreply.github.com>
2023-04-23 15:09:51 -0400

    Janice/frontend login signup update (#19)
    * endpoints for reviews
    
    Signed-off-by: Escanord <anhduy6102@gmail.com>
    
    * update checking shopOwner at login
    
    Signed-off-by: Escanord <anhduy6102@gmail.com>
    
    * update drink name in reviews
    
    Signed-off-by: Escanord <anhduy6102@gmail.com>
    
    * update user registration in backend
    
    Signed-off-by: Escanord <anhduy6102@gmail.com>
    
    * update login
    
    Signed-off-by: janicejzyh <yxj432@case.edu>
    
    * complete checking shop owner at login
    
    Signed-off-by: janicejzyh <yxj432@case.edu>
    
    * complete checking shop owner at backend
    
    Signed-off-by: janicejzyh <yxj432@case.edu>
    
    ---------
    
    Signed-off-by: Escanord <anhduy6102@gmail.com>
    Signed-off-by: janicejzyh <yxj432@case.edu>
    Co-authored-by: Escanord <anhduy6102@gmail.com>

--- BobaSpot/BobaSpot/frontend/src/temp.js
+++ BobaSpot/BobaSpot/frontend/src/temp.js
@@ -25,7 +25,8 @@ function Temp() {
   const printSessionData = () => {
     //fetching username from sesstion storage
       
-      console.log(sessionStorage.getItem("token"));
+    console.log(sessionStorage.getItem("token"));
+    console.log(sessionStorage.getItem("isShopOwner"));
       
   };
   const clearSessionData = () => {
