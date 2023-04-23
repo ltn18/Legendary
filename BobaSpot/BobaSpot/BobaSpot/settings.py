f3de40916991020c423bf997341010b3b2e30964
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-23 16:30:40 -0400

    change settings.py

--- BobaSpot/BobaSpot/BobaSpot/settings.py
+++ BobaSpot/BobaSpot/BobaSpot/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'BobaSpot.urls'
