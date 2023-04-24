af2632a2fa96be7b17cd4e1ce10eb2ee22171a64
Le Khoa Vu <vulk29@gmail.com>
2023-04-23 20:24:30 -0400

    Jest mock
    Signed-off-by: Le Khoa Vu <vulk29@gmail.com>

--- BobaSpot/BobaSpot/frontend/src/contents/landing/__test__/Map.test.js
+++ BobaSpot/BobaSpot/frontend/src/contents/landing/__test__/Map.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Map from '../Map';
+import { Button, Input, Form, InputNumber, Rate, Col, Row } from 'antd';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { StarOutlined } from '@ant-design/icons';
+import axios from "axios";
+import { Outlet, Link } from "react-router-dom";
+import { useForm } from 'antd/es/form/Form';
+/* global google */
 
-// jest.mock('axios');
-// jest.mock('@react-google-maps/api');
-// jest.mock('antd/es/form/Form');
+
+jest.mock('axios');
+jest.mock('@react-google-maps/api');
+jest.mock('antd');
+jest.mock('@ant-design/icons');
+jest.mock('react-router-dom');
 
 describe('Map', () => {
   it('renders correctly', () => {
